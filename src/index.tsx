c0c39fea6906782917b18a9550e98340b9de9288
Aleksandr Yackovlev <a.yackovlev@corp.mail.ru>
2019-12-12 23:18:21 +0300

    feat(quotes): add store for managing quotes

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <div> Test </div>;
+import quotesStore from './stores/quotes';
+
+import App from './app';
 
 const rootElement = document.getElementById('root')!;
 
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<App quotesList={quotesStore} />, rootElement);
+
+quotesStore.addQuote({
+  id: 'id1',
+  text: 'Lorem ipsum dolor amed',
+  author: 'John Smith',
+});
+
+setTimeout(() => {
+  quotesStore.addQuote({
+    id: 'id2',
+    text: 'Lorem ipsum dolor amed 1',
+    author: 'Joe Doe',
+  });
+}, 1000);
